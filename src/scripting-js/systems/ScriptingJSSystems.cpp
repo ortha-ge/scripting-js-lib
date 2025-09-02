3ae17ecb6a9f8c11aaa7a99fe81d98095cb77f08
Niamh Cuileann <sweet.niamhy@gmail.com>
2025-09-02 19:33:10 -0400

    Until I do some JS stuff disabling due to a release crash

--- src/scripting-js/systems/ScriptingJSSystems.cpp
+++ src/scripting-js/systems/ScriptingJSSystems.cpp
@@ -13,7 +13,7 @@ import Core.Scheduler;
 namespace ScriptingJS {
 
 	constexpr char TestScriptJS[] = R"(
-
+	print('Hello');
 )";
 
 	ScriptingJSSystems::ScriptingJSSystems(Core::EnTTRegistry& registry, Core::Scheduler& scheduler)
@@ -23,22 +23,23 @@ namespace ScriptingJS {
 		JSRuntime* runtime = JS_NewRuntime();
 		JSContext* context = JS_NewContext(runtime);
 
-		JSValue result = JS_Eval(context, TestScriptJS, strlen(TestScriptJS), "TestScript.js", JS_EVAL_TYPE_GLOBAL);
-		printf("%s\n", JS_ToCString(context, result));
-		if (JS_IsException(result)) {
-			printf("%s\n", JS_ToCString(context, JS_GetException(context)));
-		} else if (JS_IsFunction(context, result)) {
-			JSValue thisValue = JS_DupValue(context, result);
-			JSValue callResult = JS_Call(context, result, thisValue, 0, nullptr);
-			printf("%s\n", JS_ToCString(context, callResult));
-			JS_FreeValue(context, result);
-			JS_FreeValue(context, callResult);
-		} else {
-			printf("Failed\n");
-			JS_FreeValue(context, result);
-		}
+		// JSValue result = JS_Eval(context, TestScriptJS, strlen(TestScriptJS), "TestScript.js", JS_EVAL_TYPE_GLOBAL);
+		// printf("%s\n", JS_ToCString(context, result));
+		// if (JS_IsException(result)) {
+		// 	printf("%s\n", JS_ToCString(context, JS_GetException(context)));
+		// } else if (JS_IsFunction(context, result)) {
+		// 	// JSValue thisValue = JS_DupValue(context, result);
+		// 	// JSValue callResult = JS_Call(context, result, thisValue, 0, nullptr);
+		// 	// printf("%s\n", JS_ToCString(context, callResult));
+		// 	// JS_FreeValue(context, result);
+		// 	// JS_FreeValue(context, callResult);
+		// } else {
+		// 	printf("Failed\n");
+		// }
+		//
+		// JS_FreeValue(context, result);
+		JS_RunGC(runtime);
 
-		//JS_RunGC(runtime);
 		JS_FreeContext(context);
 		JS_FreeRuntime(runtime);
 
